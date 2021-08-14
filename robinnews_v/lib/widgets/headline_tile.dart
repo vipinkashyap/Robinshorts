4d7d47a834da8434e9c75850736183dd6ac07fb4
Vipin Kumar Kashyap <vipinkashyap2110@gmail.com>
2021-08-14 03:44:39 -0700

    Added description next to image

--- robinnews_v/lib/widgets/headline_tile.dart
+++ robinnews_v/lib/widgets/headline_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:robinnews_v/shared/utilities.dart';
 
 class HeadLineTile extends StatefulWidget {
@@ -20,39 +21,81 @@ class _HeadLineTileState extends State<HeadLineTile> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        SizedBox(
+          height: 10.0,
+        ),
         Align(
           alignment: Alignment.topLeft,
           child: Row(
             children: [
               Text(
-                widget.snapshot.data['articles'][widget.index]['source']
-                    ['name'],
-              ),
+                  widget.snapshot.data['articles'][widget.index]['source']
+                      ['name'],
+                  style: GoogleFonts.lato(fontWeight: FontWeight.bold),
+                  textScaleFactor: 0.9),
               SizedBox(
                 width: 10,
               ),
               Text(
-                Utilities().hoursBetween(
-                  DateTime.parse(widget.snapshot.data['articles'][widget.index]
-                      ['publishedAt']),
-                  DateTime.now(),
-                ),
-              ),
+                  Utilities().hoursBetween(
+                    DateTime.parse(widget.snapshot.data['articles']
+                        [widget.index]['publishedAt']),
+                    DateTime.now(),
+                  ),
+                  style: GoogleFonts.poppins(fontWeight: FontWeight.w300),
+                  textScaleFactor: 0.9),
             ],
           ),
+        ),
+        SizedBox(
+          height: 15.0,
         ),
         Align(
           alignment: Alignment.centerLeft,
           child: Text(
             widget.snapshot.data['articles'][widget.index]['title'],
+            textScaleFactor: 1.2,
+            style: GoogleFonts.notoSans(),
           ),
         ),
-        Image.network(
-          widget.snapshot.data['articles'][widget.index]['urlToImage'],
+        SizedBox(
+          height: 15.0,
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: Image.network(
+                widget.snapshot.data['articles'][widget.index]['urlToImage'],
+              ),
+            ),
+            SizedBox(
+              width: 30,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: Align(
+                alignment: Alignment.center,
+                child: Flexible(
+                  child: Text(
+                    widget.snapshot.data['articles'][widget.index]
+                        ['description'],
+                    textScaleFactor: 1,
+                    style: GoogleFonts.oxygen(),
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 6,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10.0,
         ),
         Divider(
-          color: Colors.blueGrey,
-          thickness: 1.2,
+          color: Colors.grey,
+          thickness: 0.8,
         )
       ],
     );
